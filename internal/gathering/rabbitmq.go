48ff38016d00b63e4051f5af63643440aed3c3d1
krisjanisp <krisjanispetrucena@gmail.com>
2023-09-29 14:16:32 +0300

    add placeholder for rabbitmq gatherer methods

---
+++ internal/gathering/rabbitmq.go
@@ -0,0 +1,88 @@
+package gathering
+
+import "github.com/programme-lv/tester/internal/testing"
+
+type RabbitMQGatherer struct {
+}
+
+func (r RabbitMQGatherer) StartEvaluation(testerInfo string, evalMaxScore int) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) IncrementScore(delta int) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) FinishWithInternalServerError(err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) FinishEvaluation() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) StartCompilation() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) FinishCompilation(data testing.RuntimeData) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) StartTesting() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) IgnoreTest(testId int64) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) StartTest(testId int64) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) ReportTestUserRuntimeData(testId int64, rd testing.RuntimeData) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) ReportTestUserRuntimeLimitExceeded(testId int64, flags testing.RuntimeExceededFlags) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) ReportTestUserRuntimeError(testId int64) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) ReportTestCheckerRuntimeData(testId int64, rd testing.RuntimeData) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) ReportTestVerdictAccepted(testId int64) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) ReportTestVerdictWrongAnswer(testId int64) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r RabbitMQGatherer) FinishTest(testId int64) {
+	//TODO implement me
+	panic("implement me")
+}
+
+var _ testing.EvalResGatherer = (*RabbitMQGatherer)(nil)
