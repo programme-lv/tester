2b0d554f44ba65c95c3b112558dd7ba4f16d4693
krisjanisp <krisjanispetrucena@gmail.com>
2023-10-03 20:15:55 +0300

    running isolate commands

--- internal/isolate/box.go
+++ internal/isolate/box.go
@@ -1,9 +1,10 @@
 package isolate
 
 import (
+	"fmt"
 	"io"
-	"log/slog"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -43,11 +44,62 @@ func (box *Box) Run(
 		c := DefaultConstraints()
 		constraints = &c
 	}
-	
-	box.logger.Info("running command in box", slog.String("command", command),
-		slog.String("constraints", strings.Join(constraints.ToArgs(), " ")))
 
-	return box.isolate.StartCommand(box.id, command, stdin, *constraints)
+	var process *Process = &Process{}
+
+	err := assignMetaFilePathToProcess(process)
+	if err != nil {
+		return nil, err
+	}
+
+	args := []string{"--env=HOME=/box", "--meta=" + process.metaFilePath}
+	args = append(args, constraints.ToArgs()...)
+
+	cmdStr := fmt.Sprintf(
+		"isolate --cg --box-id %d %s --run /usr/bin/env %s",
+		box.id,
+		strings.Join(args, " "),
+		command,
+	)
+
+	cmd := exec.Command("/usr/bin/bash", "-c", cmdStr)
+	cmd.Stdin = stdin
+	process.stdout, err = cmd.StdoutPipe()
+	if err != nil {
+		return process, err
+	}
+	process.stderr, err = cmd.StderrPipe()
+	if err != nil {
+		return process, err
+	}
+	process.cmd = cmd
+
+	if err = cmd.Start(); err != nil {
+		return process, err
+	}
+
+	return process, err
+}
+
+func assignMetaFilePathToProcess(process *Process) error {
+	tempFilePath, err := newTempIsolateFilePath()
+	if err != nil {
+		return err
+	}
+	process.metaFilePath = tempFilePath
+	return nil
+}
+
+func newTempIsolateFilePath() (string, error) {
+	file, err := os.CreateTemp("", "isolate.*.txt")
+	if err != nil {
+		return "", err
+	}
+	err = file.Close()
+	if err != nil {
+		return "", err
+	}
+	return file.Name(), nil
 }
 
 func (box *Box) AddFile(path string, content []byte) error {
