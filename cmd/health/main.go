21d636441fb0202981db557ed2326dae81074d47
krisjanisp <krisjanispetrucena@gmail.com>
2023-11-16 20:29:26 +0200

    progress on health endpoint

--- cmd/health/main.go
+++ cmd/health/main.go
@@ -1,14 +1,83 @@
 package main
 
 import (
+	jet "github.com/go-jet/jet/v2/postgres"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"github.com/programme-lv/tester/internal/database/proglv/public/model"
+	"github.com/programme-lv/tester/internal/database/proglv/public/table"
 	"github.com/programme-lv/tester/internal/environment"
+	"github.com/programme-lv/tester/internal/isolate"
+	"io"
 	"log"
 	"os/exec"
+	"strings"
 )
 
+type feedbackRow struct {
+	unit    string
+	health  int
+	message string
+}
+
 func main() {
+	ensureIsolateOk()
+
+	ensureLanguagesOk()
+
+}
+
+func ensureIsolateOk() {
+	isolateCmd := exec.Command("isolate", "--cg", "--cleanup")
+	log.Printf("Running %v...", isolateCmd.Args)
+	err := isolateCmd.Run()
+	panicOnError(err)
+	log.Printf("Finished %v OK", isolateCmd.Args)
+}
+
+func ensureLanguagesOk() {
+	languages := fetchLanguages()
+
+	isolateInstance := isolate.GetInstance()
+
+	for _, language := range languages {
+		box, err := isolateInstance.NewBox()
+		panicOnError(err)
+
+		err = box.AddFile(language.CodeFilename, []byte(*language.HelloWorldCode))
+		panicOnError(err)
+
+		var process *isolate.Process
+		process, err = box.Run(language.ExecuteCmd, io.NopCloser(strings.NewReader("")), nil)
+
+		stdout := process.Stdout()
+		stderr := process.Stderr()
+
+		var stdoutBytes []byte
+		stdoutBytes, err = io.ReadAll(stdout)
+		panicOnError(err)
+		stdoutStr := string(stdoutBytes)
+
+		var stderrBytes []byte
+		stderrBytes, err = io.ReadAll(stderr)
+		panicOnError(err)
+		stderrStr := string(stderrBytes)
+
+		var metrics *isolate.Metrics
+		metrics, err = process.Wait()
+		panicOnError(err)
+
+		if metrics.ExitCode != 0 {
+			log.Printf("Language %v failed to run. Exit code: %v. Stdout: %v. Stderr: %v", language.FullName, metrics.ExitCode, stdoutStr, stderrStr)
+		} else {
+			log.Printf("Language %v ran OK. Stdout: %v. Stderr: %v", language.FullName, stdoutStr, stderrStr)
+		}
+		// create isolate bo
+		//ensureLanguageOk(language)
+	}
+}
+
+func fetchLanguages() []model.ProgrammingLanguages {
 	cfg := environment.ReadEnvConfig()
 
 	log.Println("Connecting to Postgres...")
@@ -20,15 +89,16 @@ func main() {
 	}(postgres)
 	log.Println("Connected to Postgres")
 
-	ensureIsolateOk()
-}
+	var res []model.ProgrammingLanguages
 
-func ensureIsolateOk() {
-	isolateCmd := exec.Command("isolate", "--cg", "--cleanup")
-	log.Printf("Running %v...", isolateCmd.Args)
-	err := isolateCmd.Run()
+	log.Println("Selecting languages...")
+	err = jet.SELECT(table.ProgrammingLanguages.AllColumns).
+		FROM(table.ProgrammingLanguages).
+		Query(postgres, &res)
 	panicOnError(err)
-	log.Printf("Finished %v OK", isolateCmd.Args)
+	log.Printf("Selected %v languages", len(res))
+
+	return res
 }
 
 func panicOnError(err error) {
