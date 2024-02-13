81468ab83cbfccf37fc188028adca5847cefd19a
krisjanisp <krisjanispetrucena@gmail.com>
2024-02-13 23:37:08 +0200

    update evaluation request

--- pkg/messaging/request.go
+++ pkg/messaging/request.go
@@ -1,18 +1,44 @@
 package messaging
 
 type Submission struct {
-	SourceCode string
-	LanguageId string
+	Body   string `json:"body"`
+	LangID string `json:"lang_id"`
+}
+
+type Limits struct {
+	CPUTimeMillis int `json:"cpu_time_millis"`
+	MemKibibytes  int `json:"mem_kibibytes"`
+}
+
+type Test struct {
+	ID        int    `json:"id"`
+	InSHA256  string `json:"in_sha256"`
+	AnsSHA256 string `json:"ans_sha256"`
+}
+
+type TestResolver struct {
+	Type      string `json:"type"`
+	AccessKey string `json:"access_key"`
+	SecretKey string `json:"secret_key"`
+}
+
+type Subtask struct {
+	ID      int   `json:"id"`
+	Score   int   `json:"score"`
+	TestIDs []int `json:"test_ids"`
 }
 
 type EvaluationRequest struct {
-	TaskVersionId int64
-	Submission    Submission
+	Submission     Submission   `json:"submission"`
+	Limits         Limits       `json:"limits"`
+	EvalTypeID     string       `json:"eval_type_id"`
+	Tests          []Test       `json:"tests"`
+	TestResolver   TestResolver `json:"test_resolver"`
+	Subtasks       []Subtask    `json:"subtasks"`
+	TestlibChecker *string      `json:"testlib_checker"`
 }
 
-type Correlation struct {
-	HasEvaluationId bool  `json:"has_evaluation_id"`
-	EvaluationId    int64 `json:"evaluation_id,omitempty"`
-	UnixMillis      int64 `json:"unix_millis"`
-	RandomInt63     int64 `json:"random_int_63"`
+type ResponseCorrelation struct {
+	UnixMillis  int64 `json:"unix_millis"`
+	RandomInt63 int64 `json:"random_int_63"`
 }
