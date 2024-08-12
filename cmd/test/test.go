b26cec559bd812ccaa6b09b86b63ca2af9478242
krisjanisp <krisjanispetrucena@gmail.com>
2024-08-12 23:07:34 +0300

    succesfully downloaded tests

--- cmd/test/test.go
+++ cmd/test/test.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net/url"
 	"os"
 	"path/filepath"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/klauspost/compress/zstd"
 	"github.com/programme-lv/tester/internal"
 	"github.com/programme-lv/tester/internal/checkers"
 	"github.com/programme-lv/tester/internal/filestore"
@@ -14,7 +21,7 @@ import (
 )
 
 func main() {
-	filestore := filestore.NewFileStore(s3DownloadFunc)
+	filestore := filestore.NewFileStore(getS3DownloadFunc())
 	filestore.StartDownloadingInBg()
 	tlibCheckers := checkers.NewTestlibCheckerCompiler()
 	tester := tester.NewTester(filestore, tlibCheckers)
@@ -29,14 +36,66 @@ func main() {
 
 }
 
-func s3DownloadFunc(s3Uri string, path string) error {
-	// write hello world to path
-	err := os.WriteFile(path, []byte("hello world"), 0777)
+func getS3DownloadFunc() func(s3Uri string, path string) error {
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("eu-central-1"),
+		config.WithSharedConfigProfile("kp"),
+	)
 	if err != nil {
-		return fmt.Errorf("failed to write to file %s: %w", path, err)
+		panic(fmt.Sprintf("unable to load SDK config, %v", err))
+	}
+	s3Client := s3.NewFromConfig(cfg)
+
+	return func(s3Uri string, path string) error {
+		u, err := url.Parse(s3Uri)
+		if err != nil {
+			return fmt.Errorf("failed to parse s3 uri %s: %w", s3Uri, err)
+		}
+
+		if u.Scheme != "s3" {
+			return fmt.Errorf("invalid s3 uri scheme: %s", u.Scheme)
+		}
+
+		out, err := os.Create(path)
+		if err != nil {
+			return fmt.Errorf("failed to create file %s: %w", path, err)
+		}
+		defer out.Close()
+
+		log.Printf("Downloading file %s from s3", s3Uri)
+		obj, err := s3Client.GetObject(context.TODO(), &s3.GetObjectInput{
+			Bucket: aws.String(u.Host),
+			Key:    aws.String(u.Path[1:]),
+		})
+		if err != nil {
+			return fmt.Errorf("failed to download file %s from s3: %w (host: %s, path: %s)", s3Uri, err, u.Host, u.Path)
+		}
+
+		if (obj.ContentType != nil && *obj.ContentType == "application/zstd") ||
+			filepath.Ext(u.Path) == ".zst" {
+
+			d, err := zstd.NewReader(obj.Body)
+			if err != nil {
+				return fmt.Errorf("failed to create zstd reader: %w", err)
+			}
+			defer d.Close()
+			_, err = io.Copy(out, d)
+			if err != nil {
+				return fmt.Errorf("failed to write file %s: %w", path, err)
+			}
+
+			return nil
+		}
+
+		_, err = io.Copy(out, obj.Body)
+		if err != nil {
+			return fmt.Errorf("failed to write file %s: %w", path, err)
+		}
+
+		return nil
 	}
 
-	return nil
 }
 
 type stdoutGathererMock struct {
